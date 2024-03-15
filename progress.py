aab069c5c030ad25d6cf40cf6983634d096f07ba
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2024-03-15 17:32:22 +0100

    First working version

---
+++ progress.py
@@ -0,0 +1,63 @@
+def scale(value, start, end):
+    return start + value * (end - start) / 100
+
+
+def naive_scaled(it, start, end):
+    return (scale(value, start, end) for value in it)
+
+
+def bound_scale(it):
+    """
+    Ensures that:
+    * values are bound between [0, 100]
+    * value is never less than previous value
+    * last value is 100
+    """
+    last = 0
+    for value in it:
+        value = min(max(last, value), 100)
+        yield value
+        last = value
+    if last < 100:
+        yield 100
+
+
+def safe_scaled(it, start, end):
+    return naive_scaled(bound_scale(it), start, end)
+
+
+# ---------------------
+
+def task_1():
+    yield 1
+    yield 30
+    yield 75
+    yield 100
+
+
+def task_2():
+    yield from range(10, 100, 21)
+
+
+def task_3():
+    yield -5
+    yield 55
+    yield 23
+    yield 98
+
+
+def main():
+    yield 10
+
+    yield from safe_scaled(task_1(), 10, 30)
+
+    yield from safe_scaled(range(10, 100, 25), 30, 45)
+
+    yield from safe_scaled(task_3(), 45, 80)
+    yield 80
+    yield 99
+
+
+if __name__ == "__main__":
+    for i in safe_scaled(main(), 0, 100):
+        print(i)
