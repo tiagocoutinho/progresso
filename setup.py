570eaf244b9ab44f8290d3af40b116bcdd82682f
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2024-03-16 07:07:35 +0100

    Add license and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#
+# This file is part of the progresso project
+#
+# Copyright (c) 2024 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+"""The setup script."""
+
+from setuptools import setup
+
+setup()
